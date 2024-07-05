8abcd95817ac92aac7163938dbb3673923828d92
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2024-07-05 17:45:39 +0530

    Update exportToCsv.sh

--- exportToCsv.sh
+++ exportToCsv.sh
@@ -4,31 +4,6 @@ exp=/opt/Experiments
 opFile=$exp/exp-data.csv
 rm -f $opFile
 
-declare -a experimentDirAry=("allSecure" "coreSecure" "pfcpSecure" "ranSecure" "unsecured" "istioSec")
-
-echo "experiment,numSessions,run,UePassed,UeFailed,AverageTotalRegTime,AverageRegReqAuthReq,AverageAuthRspSecMReq,AverageSecModeRspICReq,AverageTotalPduEstTime,AveragePduSessReqAccept,amfQueueLength,smfQueueLength,upfQueueLength,amfTimeTaken,smfTimeTaken,upfTimeTaken" >> $opFile
-for f1 in "${experimentDirAry[@]}"
-do  
-    for subexp in `seq 100 100 1000`
-    do
-        for j in `seq 1 1 10`
-        do
-            gnbLogFIle=$exp/$f1-$j/$subexp/gnb.log_node5
-            nfFile=$exp/$f1-$j/$subexp/nf_max_queue.txt
-            if [ -f "$gnbLogFIle" ] && [ -f "$nfFile" ]; then
-                gnbRes=$(parse_log_file $gnbLogFIle)
-                ueSessCount=$(cat $ueipn1File | wc -l)
-                amfQueueLength=$(cat $nfFile | grep amf | cut -d "," -f2)
-                smfQueueLength=$(cat $nfFile | grep smf | cut -d "," -f2)
-                upfQueueLength=$(cat $nfFile | grep upf | cut -d "," -f2)
-                amfTimeTaken=$(printf '%.9f\n' $(cat $nfFile | grep amf | cut -d "," -f3))
-                smfTimeTaken=$(printf '%.9f\n' $(cat $nfFile | grep smf | cut -d "," -f3))
-                upfTimeTaken=$(printf '%.9f\n' $(cat $nfFile | grep upf | cut -d "," -f3))
-                echo "$f1,$subexp,$j,$gnbRes,$amfQueueLength,$smfQueueLength,$upfQueueLength,$amfTimeTaken,$smfTimeTaken,$upfTimeTaken" >> $opFile
-            fi
-        done
-    done
-done
 
 # Function to parse log file and return required data
 parse_log_file() {
@@ -102,3 +77,31 @@ parse_log_file() {
 
     echo "$UePassed,$UeFailed,$AverageTotalRegTime,$AverageRegReqAuthReq,$AverageAuthRspSecMReq,$AverageSecModeRspICReq,$AverageTotalPduEstTime,$AveragePduSessReqAccept"
 }
+
+
+declare -a experimentDirAry=("allSecure" "coreSecure" "pfcpSecure" "ranSecure" "unsecured" "istioSec")
+
+echo "experiment,numSessions,run,UePassed,UeFailed,AverageTotalRegTime,AverageRegReqAuthReq,AverageAuthRspSecMReq,AverageSecModeRspICReq,AverageTotalPduEstTime,AveragePduSessReqAccept,amfQueueLength,smfQueueLength,upfQueueLength,amfTimeTaken,smfTimeTaken,upfTimeTaken" >> $opFile
+for f1 in "${experimentDirAry[@]}"
+do  
+    for subexp in `seq 100 100 1000`
+    do
+        for j in `seq 1 1 10`
+        do
+            gnbLogFIle=$exp/$f1-$j/$subexp/gnb.log_node5
+            nfFile=$exp/$f1-$j/$subexp/nf_max_queue.txt
+            if [ -f "$gnbLogFIle" ] && [ -f "$nfFile" ]; then
+                gnbRes=$(parse_log_file $gnbLogFIle)
+                ueSessCount=$(cat $ueipn1File | wc -l)
+                amfQueueLength=$(cat $nfFile | grep amf | cut -d "," -f2)
+                smfQueueLength=$(cat $nfFile | grep smf | cut -d "," -f2)
+                upfQueueLength=$(cat $nfFile | grep upf | cut -d "," -f2)
+                amfTimeTaken=$(printf '%.9f\n' $(cat $nfFile | grep amf | cut -d "," -f3))
+                smfTimeTaken=$(printf '%.9f\n' $(cat $nfFile | grep smf | cut -d "," -f3))
+                upfTimeTaken=$(printf '%.9f\n' $(cat $nfFile | grep upf | cut -d "," -f3))
+                echo "$f1,$subexp,$j,$gnbRes,$amfQueueLength,$smfQueueLength,$upfQueueLength,$amfTimeTaken,$smfTimeTaken,$upfTimeTaken" >> $opFile
+            fi
+        done
+    done
+done
+
