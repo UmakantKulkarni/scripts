b51aa64c64172ab2a375e9d3d4b3986548579e0d
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2024-08-25 23:55:00 -0400

    MilCom fall 2024 Plots

--- plotOverleaf.py
+++ plotOverleaf.py
@@ -13,7 +13,7 @@ import seaborn as sns
 
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
-sns.set_context(context="paper", font_scale=1.6)
+sns.set_context(context="paper", font_scale=2)
 file_format = "pdf"
 
 my_dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -47,7 +47,7 @@ colors = list(cmap.colors)
 color_plte = sns.color_palette("tab10")
 
 
-if 1:
+if 0:
     exp_plot_order = ["UnSecured", "RAN Secured", "PFCP Secured", "Core Secured", "All Secured","Istio"]
     exp_labels = exp_plot_order
 
@@ -55,7 +55,7 @@ if 1:
     flatui = colors[:len(exp_plot_order)]
     #sns.set_palette(flatui)
 
-    fig, ax = plt.subplots(figsize=(12,5))
+    fig, ax = plt.subplots(figsize=(10,6))
     sb = sns.barplot(data=df_plot,
                      x='numSessions',
                      y='amfTimeTaken',
@@ -75,16 +75,46 @@ if 1:
     sb.clear()
     plt.close()
 
+    # Calculate the improvement in session setup time for GTX compared to STO
+    df_improvement = df_plot.pivot_table(index='numSessions', columns='mode', values='amfTimeTaken', aggfunc='mean')
+    df_improvement['Improvement (%)'] = ((df_improvement['All Secured'] - df_improvement['UnSecured']) / df_improvement['All Secured']) * 100
+
+    # Print the improvement for each simultaneous request
+    print("Improvement for each simultaneous request:")
+    print(df_improvement[['Improvement (%)']])
+
+    # Calculate the overall improvement by averaging the improvements
+    overall_improvement = df_improvement['Improvement (%)'].mean()
+
+    # Print the overall improvement
+    print("\nOverall increase of All Secured amfTimeTaken over UnSecured (%):", overall_improvement)
+
+    # Calculate the improvement in session setup time for GTX compared to STO
+    df_improvement = df_plot.pivot_table(index='numSessions', columns='mode', values='amfTimeTaken', aggfunc='mean')
+    df_improvement['Improvement (%)'] = ((df_improvement['Istio'] - df_improvement['UnSecured']) / df_improvement['Istio']) * 100
+
+    # Print the improvement for each simultaneous request
+    print("Improvement for each simultaneous request:")
+    print(df_improvement[['Improvement (%)']])
+
+    # Calculate the overall improvement by averaging the improvements
+    overall_improvement = df_improvement['Improvement (%)'].mean()
+
+    # Print the overall improvement
+    print("\nOverall increase of Istio amfTimeTaken over UnSecured (%):", overall_improvement)
+
+
 if 1:
     exp_plot_order = ["Core Secured", "Istio"]
     exp_labels = ["ZTX-SEM", "Istio"]
 
     df_plot = df[df['UePassed'] >= 0.8 * df['numSessions']]
+    df_plt_filtered = df_plot[df_plot['numSessions'].isin([200, 400, 600, 800, 1000])]
     flatui = colors[:len(exp_plot_order)]
     #sns.set_palette(flatui)
 
-    fig, ax = plt.subplots(figsize=(8,5))
-    sb = sns.barplot(data=df_plot,
+    fig, ax = plt.subplots()
+    sb = sns.barplot(data=df_plt_filtered,
                      x='numSessions',
                      y='amfTimeTaken',
                      hue='mode',
@@ -103,8 +133,22 @@ if 1:
     sb.clear()
     plt.close()
 
-    fig, ax = plt.subplots(figsize=(8,5))
-    sb = sns.barplot(data=df_plot,
+    # Calculate the improvement in session setup time for GTX compared to STO
+    df_improvement = df_plot.pivot_table(index='numSessions', columns='mode', values='amfTimeTaken', aggfunc='mean')
+    df_improvement['Improvement (%)'] = ((df_improvement['Istio'] - df_improvement['Core Secured']) / df_improvement['Istio']) * 100
+
+    # Print the improvement for each simultaneous request
+    print("Improvement for each simultaneous request:")
+    print(df_improvement[['Improvement (%)']])
+
+    # Calculate the overall improvement by averaging the improvements
+    overall_improvement = df_improvement['Improvement (%)'].mean()
+
+    # Print the overall improvement
+    print("\nOverall Improvement amfTimeTaken over Istio (%):", overall_improvement)
+
+    fig, ax = plt.subplots()
+    sb = sns.barplot(data=df_plt_filtered,
                      x='numSessions',
                      y='AverageRegTime',
                      hue='mode',
@@ -123,14 +167,28 @@ if 1:
     sb.clear()
     plt.close()
 
-    fig, ax = plt.subplots(figsize=(8,5))
-    sb = sns.barplot(data=df_plot,
+    # Calculate the improvement in session setup time for GTX compared to STO
+    df_improvement = df_plot.pivot_table(index='numSessions', columns='mode', values='AverageRegTime', aggfunc='mean')
+    df_improvement['Improvement (%)'] = ((df_improvement['Istio'] - df_improvement['Core Secured']) / df_improvement['Istio']) * 100
+
+    # Print the improvement for each simultaneous request
+    print("Improvement for each simultaneous request:")
+    print(df_improvement[['Improvement (%)']])
+
+    # Calculate the overall improvement by averaging the improvements
+    overall_improvement = df_improvement['Improvement (%)'].mean()
+
+    # Print the overall improvement
+    print("\nOverall Improvement AverageRegTime over Istio (%):", overall_improvement)
+
+    fig, ax = plt.subplots()
+    sb = sns.barplot(data=df_plt_filtered,
                      x='numSessions',
                      y='AveragePduEstTime',
                      hue='mode',
                      palette=color_plte,capsize=.1,
                      hue_order=exp_plot_order)
-    plt.ylabel('PDU Session Establishment Time (s)')
+    plt.ylabel('PDU Session Establishment Time (s)',fontsize=15)
     plt.xlabel("Simultaneous Requests")
     sb.legend_.set_title(None)
     plt.tight_layout()
@@ -143,7 +201,21 @@ if 1:
     sb.clear()
     plt.close()
 
-    if 1:
+    # Calculate the improvement in session setup time for GTX compared to STO
+    df_improvement = df_plot.pivot_table(index='numSessions', columns='mode', values='AveragePduEstTime', aggfunc='mean')
+    df_improvement['Improvement (%)'] = ((df_improvement['Istio'] - df_improvement['Core Secured']) / df_improvement['Istio']) * 100
+
+    # Print the improvement for each simultaneous request
+    print("Improvement for each simultaneous request:")
+    print(df_improvement[['Improvement (%)']])
+
+    # Calculate the overall improvement by averaging the improvements
+    overall_improvement = df_improvement['Improvement (%)'].mean()
+
+    # Print the overall improvement
+    print("\nOverall Improvement AveragePduEstTime over Istio (%):", overall_improvement)
+
+    if 0:
         for nf in ["amf", "smf"]:
             param = "{}QueueLength".format(nf)
             sb = sns.barplot(data=df_plot,
