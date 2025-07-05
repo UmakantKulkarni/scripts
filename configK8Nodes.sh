8307731dab487c19778e1257e0d90ee14a903fd9
Umakant Kulkarni <UmakantKulkarni@users.noreply.github.com>
2025-07-04 21:59:23 -0400

    Update configK8Nodes.sh

--- configK8Nodes.sh
+++ configK8Nodes.sh
@@ -23,7 +23,7 @@ pip3 install -U h2 pyaml flask scapy
 
 #install go - https://github.com/HewlettPackard/PacketRusher/wiki/Installation
 cd $my_dir
-go_ver=1.23.4
+go_ver=1.24.4
 # Warning this command will remove your existing local Go installation if you have one
 wget https://go.dev/dl/go${go_ver}.linux-amd64.tar.gz && sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go${go_ver}.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
