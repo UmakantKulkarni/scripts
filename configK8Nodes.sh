df18b6ae9f118fc3e11376ade57a123bb3dadd78
Umakant Kulkarni <UmakantKulkarni@users.noreply.github.com>
2025-07-12 01:41:11 -0400

    Update configK8Nodes.sh

--- configK8Nodes.sh
+++ configK8Nodes.sh
@@ -8,6 +8,9 @@ my_dir=/opt
 
 cd $my_dir
 
+sudo sysctl fs.inotify.max_user_watches=524288
+sudo sysctl fs.inotify.max_user_instances=512
+
 DEBIAN_FRONTEND=noninteractive apt-get -y update
 DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 DEBIAN_FRONTEND=noninteractive apt-get -y update
